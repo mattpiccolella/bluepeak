5aab1501481b22b4882de65246e938748fabc06b
Matt Piccolella <mattpiccolella1@gmail.com>
2024-01-20 15:22:02 -0800

    Basic backend

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -5,3 +5,11 @@ class Conversation(db.Model):
     prompt = db.Column(db.String(128), nullable=False)
     response = db.Column(db.String(1280), nullable=True)
     # Add other fields here
+
+    def serialize(self):
+        # Convert the object's state to a serializable dictionary
+        return {
+            'id': self.id,
+            'prompt': self.prompt,
+            'response': self.response
+        }
--- backend/routes/api.py
+++ backend/routes/api.py
@@ -1,15 +1,11 @@
+from crypt import methods
 from flask import Blueprint, jsonify, request
 from models import Conversation
 from extensions import db
 
 api = Blueprint('api', __name__)
 
-@api.route('/data')
-def get_data():
-    return jsonify({"data": "Hello from Flask! I'm testing a deploy change here"})
-
-
-@api.route('/conversation/add', methods=['POST'])
+@api.route('/conversations', methods=['POST'])
 def add_conversation():
     request_data = request.get_json()
 
@@ -21,3 +17,18 @@ def add_conversation():
     db.session.commit()
 
     return jsonify({"message": "Conversation added successfully!"})
+
+@api.route('/conversations', methods=['GET'])
+def get_conversations():
+    conversations = Conversation.query.all()
+
+    return jsonify({"conversations": [conversation.serialize() for conversation in conversations]})
+
+@api.route('/conversations/<int:item_id>', methods=['GET'])
+def get_conversation_by_id(item_id):
+    conversation = Conversation.query.filter_by(id=item_id).first()
+
+    if conversation is None:
+        return jsonify({"message": "Conversation not found!"}), 404
+
+    return jsonify({"conversation": conversation.serialize()})
