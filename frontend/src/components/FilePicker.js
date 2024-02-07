6b64d93a161fd6960aeeac1b06888d663e8ec104
Matt Piccolella <mattpiccolella1@gmail.com>
2024-02-06 21:43:52 -0800

    Slight upgrade to file picker

--- frontend/src/components/FilePicker.js
+++ frontend/src/components/FilePicker.js
@@ -3,15 +3,16 @@ import React, { useState } from 'react';
 const FileList = ({ files, selectedFiles, onFileSelect }) => {
     return (
         <div>
-            <h2>Pick files</h2>
+            <h2 className="text-xl font-semibold text-center text-gray-800 mb-2">Select files</h2>
             {files.map(file => (
-                <div key={file.id}>
+                <div key={file.id} className='mb-1'>
                     <input
                         type="checkbox"
                         checked={selectedFiles.includes(file.id)}
                         onChange={() => onFileSelect(file)}
+                        className="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
                     />
-                    {file.file_name}
+                    <span className="text-gray-700 ml-2">{file.file_name}</span>
                 </div>
             ))}
         </div>
